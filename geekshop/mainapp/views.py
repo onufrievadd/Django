09e5b46063dea7017199c0e2db3cdce6ee97a1ee
Onufrieva Daria <onyfrievadany@mail.ru>
2021-11-30 14:14:21 +0300

    done

---
+++ geekshop/mainapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from mainapp.models import Product, ProductCategory
+# import json
+# import os
+
+# MODULE_DIR =os.path.dirname(__file__)
+# Create your views here.
+
+
+def index(request):
+    context = {
+        'title': 'Geekshop', }
+    return render(request, 'mainapp/index.html', context)
+
+
+def products(request):
+    # file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+    context = {
+        'title': 'Geekshop - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all()
+    }
+
+    # context['products'] = json.load(open(file_path, encoding='utf-8'))
+    return render(request, 'mainapp/products.html', context)
